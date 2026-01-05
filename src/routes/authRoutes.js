5408abc0825f25e9dfa32704be37ba3fcde5580b
HamsyDev <mach.hamsy@gmail.com>
2026-01-06 05:14:42 +0700

    delete file

--- src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-// import express from "express";
-// import { login, updateLogin } from "../controllers/authController.js";
-
-// const router = express.Router();
-
-// router.post("/login", login);
-// router.put("/login", updateLogin);
-
-// export default router;
